c40537ccf8a559b4e39fc3d5b3edc6f6444f519f
kagan019 <70962482+kagan019@users.noreply.github.com>
2022-01-14 05:33:46 -0600

    phew

--- day17/trick_shot.jl
+++ day17/trick_shot.jl
@@ -1,5 +1,5 @@
 begin
-input = read("day17/sample.txt", String)
+input = read("day17/input.txt", String)
 pattern = r"target area: x=(-?[0-9]*)..(-?[0-9]*), y=(-?[0-9]*)..(-?[0-9]*)"
 cap = match(pattern, input).captures
 @assert nothing ∉ cap
@@ -125,24 +125,93 @@ end
 valid_xvels
 end
 
+function simy(init_yvel,steps)
+    ypos = [0]
+    v = init_yvel
+    for i=1:steps
+        push!(ypos,ypos[end]+v)
+        v -= 1
+    end
+    ypos
+end
+
 #of those, find the ysteps that land in the target area
-begin
-maxyvel = -Inf
-for (xvel,stps)=valid_xvels|>collect
-    for yv=targetY[1]:targetY[2]
-        for stp=stps |> collect
-            stationary = xvel == stp # due to drag
-            if stationary 
-                #find the maximum and solve there
-            else
-                #must reach yv after exactly stp steps
-                yvel = (yv + arith(stp-1)) / stp
-                if isinteger(yvel)
-                    maxyvel = max(maxyvel,yv)
+function maxy()
+    my = -Inf
+    for (xvel,stps)=valid_xvels|>collect
+        for yv=targetY[1]:targetY[2]
+            for stp=stps |> collect
+                stationary = xvel == stp # due to drag
+                if stationary 
+                    for k=-stp*10:stp*10
+                        s = simy(k,5000)
+                        if yv in s
+                            i = findfirst(s .== yv)
+                            if !isnothing(i)
+                                my = max(my, s[1:i]...)
+                            end
+                        end
+                    end
+                else
+                    #must reach yv after exactly stp steps
+                    yvel = (yv + arith(stp-1)) / stp
+                    if isinteger(yvel)
+                        my = max(my, begin
+                            yp = simy(yvel,stp)
+                            @assert yp[end] == yv
+                            my = max(my,yp...)
+                        end)
+                    end
                 end
             end
         end
     end
+    my
 end
-maxyvel
+
+exmp = begin
+[
+    (23,-10),  (25,-9 ),  (27,-5 ),  (29,-6 ),  (22,-6 ),  (21,-7 ), ( 9,0  ),   (27,-7 ),  (24,-5
+),    (25,-7 ),  (26,-6 ),  (25,-5 ), ( 6,8  ),   (11,-2 ),  (20,-5 ),  (29,-10), ( 6,3  ),   (28,-7
+),   ( 8,0  ),   (30,-6 ),  (29,-8 ),  (20,-10), ( 6,7  ),  ( 6,4  ),  ( 6,1  ),   (14,-4 ),  (21,-6
+),    (26,-10), ( 7,-1 ),  ( 7,7  ),  ( 8,-1 ),   (21,-9 ), ( 6,2  ),   (20,-7 ),  (30,-10),  (14,-3
+),    (20,-8 ),  (13,-2 ), ( 7,3  ),   (28,-8 ),  (29,-9 ),  (15,-3 ),  (22,-5 ),  (26,-8 ),  (25,-8
+),    (25,-6 ),  (15,-4 ), ( 9,-2 ),   (15,-2 ),  (12,-2 ),  (28,-9 ),  (12,-3 ),  (24,-6 ),  (23,-7
+),    (25,-10), ( 7,8  ),   (11,-3 ),  (26,-7 ), ( 7,1  ),   (23,-9 ), ( 6,0  ),   (22,-10),  (27,-6
+),   ( 8,1  ),   (22,-8 ),  (13,-4 ), ( 7,6  ),   (28,-6 ),  (11,-4 ),  (12,-4 ),  (26,-9 ), ( 7,4
+ ),   (24,-10),  (23,-8 ),  (30,-8 ), ( 7,0  ),  ( 9,-1 ),   (10,-1 ),  (26,-5 ),  (22,-9 ), ( 6,5
+ ),  ( 7,5  ),   (23,-6 ),  (28,-10),  (10,-2 ),  (11,-1 ),  (20,-9 ),  (14,-2 ),  (29,-7 ),  (13,-3
+),    (23,-5 ),  (24,-8 ),  (27,-9 ),  (30,-7 ),  (28,-5 ),  (21,-10), ( 7,9  ),  ( 6,6  ),   (21,-5
+),    (27,-10), ( 7,2  ),   (30,-9 ),  (21,-8 ),  (22,-7 ),  (24,-9 ),  (20,-6 ), ( 6,9  ),   (29,-5
+),   ( 8,-2 ),   (27,-8 ),  (30,-5 ),  (24,-7
+)]
+end |> Set
+
+function countv()
+    vels = Set()
+    for (xvel,stps)=valid_xvels|>collect
+        for yv=targetY[1]:targetY[2]
+            for stp=stps |> collect
+                stationary = xvel == stp # due to drag
+                if stationary 
+                    for k=-stp*100:stp*100
+                        s = simy(k,5000)[stp+1:end] #at or beyond when the missile
+                                                    #starts dropping down
+                        if yv in s
+                            push!(vels,(xvel,k))
+                        end
+                    end
+                else
+                    #must reach yv after exactly stp steps
+                    yvel = (yv + arith(stp-1)) / stp
+                    if isinteger(yvel)
+                        push!(vels,(xvel,yvel))
+                    end
+                end
+            end
+        end
+    end
+    vels
 end
+
+countv() |> length
